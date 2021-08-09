a39ad59cd230f07b89827823442ac8e4426b7cc7
bmCDV <bmikolajczak@edu.cdv.pl>
2021-08-10 01:04:06 +0200

    finished validation in modal

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,10 +1,11 @@
 import {
-  Form as VeeForm, Field as VeeField, defineRule, ErrorMessage,
+  Form as VeeForm, Field as VeeField, defineRule, ErrorMessage, configure,
 } from 'vee-validate';
 import {
   required, min, max, alpha_spaces as alphaSpaces, alpha_num as alphaNum, email, numeric, confirmed,
   max_value as maxValue,
   min_value as minValue,
+  not_one_of as excluded,
 } from '@vee-validate/rules';
 
 export default {
@@ -13,6 +14,7 @@ export default {
     app.component('VeeField', VeeField);
     app.component('ErrorMessage', ErrorMessage);
     defineRule('required', required);
+    defineRule('tos', required);
     defineRule('min', min);
     defineRule('max', max);
     defineRule('alpha_spaces', alphaSpaces);
@@ -21,6 +23,35 @@ export default {
     defineRule('numeric', numeric);
     defineRule('max_value', maxValue);
     defineRule('min_value', minValue);
-    defineRule('confirmed', confirmed);
+    defineRule('passwords_mismatch', confirmed);
+    defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required.`,
+          min: `The field ${ctx.field} is too short.`,
+          max: `The field ${ctx.field} is too long.`,
+          alpha_spaces: `The field ${ctx.field} may only contain alphabetic characters and spaces.`,
+          email: `The field ${ctx.field} must be a vaild email`,
+          numeric: `The field ${ctx.field} may only be a numeric value.`,
+          min_value: `The field ${ctx.field} is too low.`,
+          max_value: `The field ${ctx.field} is too high.`,
+          excluded: `You are not allowed to use this value for the field ${ctx.field}.`,
+          country_excluded: 'Due to restrictions we do not allow users from this locaton.',
+          passwords_mismatch: 'The passwords don\'t match.',
+          tos: 'You must accept the terms of service.',
+        };
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid`;
+        return message;
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
+    });
   },
 };
