d8bb9cd8120a4a152c4c54848c078f58add86c95
bmikolajczak <br.mikolajczak@gmail.com>
2021-08-27 22:28:39 +0200

    title of the song display fix

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -11,7 +11,7 @@
       </div>
       <!-- Current Position -->
       <div class="float-left w-7 h-7 leading-3 text-gray-400 mt-0 text-lg w-14 ml-5 mt-1">
-        <span class="player-currenttime">{{ seek() }}</span>
+        <span class="player-currenttime">{{ seek }}</span>
       </div>
       <!-- Scrub -->
       <div class="float-left w-7 h-7 leading-3 ml-7 mt-2 player-scrub">
@@ -25,17 +25,17 @@
         @click.prevent="updateSeek">
           <!-- Player Ball -->
           <span class="absolute top-neg-8 text-gray-800 text-lg"
-          :style="{left: playerProgress()}">
+          :style="{left: playerProgress}">
             <i class="fas fa-circle"></i>
           </span>
           <!-- Player Progress Bar-->
           <span class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
-            :style="{width: playerProgress()}"></span>
+            :style="{width: playerProgress}"></span>
         </span>
       </div>
       <!-- Duration -->
       <div class="float-left w-7 h-7 leading-3 text-gray-400 mt-0 text-lg w-14 ml-8 mt-1">
-        <span class="player-duration">{{ duration() }}</span>
+        <span class="player-duration">{{ duration }}</span>
       </div>
     </div>
   </div>
@@ -48,10 +48,11 @@ export default {
   name: 'Player',
   methods: {
     ...mapActions(['toggleAudio', 'updateSeek']),
-    ...mapState(['duration', 'seek', 'playerProgress', 'currentSong']),
+
   },
   computed: {
     ...mapGetters(['playing']),
+    ...mapState(['duration', 'seek', 'playerProgress', 'currentSong']),
   },
 };
 </script>
